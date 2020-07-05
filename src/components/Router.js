63cb0752dab5bb3708c7fbb2739ddda69b3224ff
João Costa <joaocostajjsc@gmail.com>
2020-07-05 12:36:26 +0100

    deleted files to have a single page app

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
-import Effects from './Effects';
-import Moods from './Moods';
+
 // import NotFound from './NotFound';
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
              <Route exact path ="/" component={App} />
-             <Route exact path ="/effects" component={Effects} />
-             <Route exact path ="/moods" component={Moods} />
              {/* quando nao tem path vai para 404 */}
              {/* <Route exact component={NotFound} /> */}
         </Switch>
